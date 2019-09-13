2d84ba3dfdc81612ab89ba2e05f60513acd62113
Mick <mhearne@redhat.com>
2019-09-13 10:56:36 +0100

    adding app centre

--- cicd/Jenkinsfile.groovy
+++ cicd/Jenkinsfile.groovy
@@ -1,53 +1,53 @@
 pipeline {
-  agent {
-    node {
-      label 'nodejs' 
+    agent {
+        node {
+            label 'nodejs'
+        }
     }
-  }
-  options {
-    timeout(time: 20, unit: 'MINUTES') 
-  }
-  stages {
-    stage('prepare') {
-        steps {
-            script {
-                openshift.withCluster() {
-                    openshift.withProject() {
-                        echo "prepare"
-                        echo "Using project: ${openshift.project()}"
+    options {
+        timeout(time: 20, unit: 'MINUTES')
+    }
+    stages {
+        stage('prepare') {
+            steps {
+                script {
+                    openshift.withCluster() {
+                        openshift.withProject() {
+                            echo "prepare"
+                            echo "Using project: ${openshift.project()}"
+                        }
                     }
                 }
             }
         }
-    }
-    stage('clone') {
-      steps {
-        script {
+        stage('clone') {
+            steps {
+                script {
                     echo "clone"
+                }
+            }
         }
-      }
-    }
-    stage('prebuild') {
-      steps {
-        script {
+        stage('prebuild') {
+            steps {
+                script {
                     echo "prebuild"
+                }
+            }
         }
-      }
-    }
-    stage('build') {
-      steps {
-        script {
-                  //expo build apk
+        stage('build') {
+            steps {
+                script {
+                    //expo build apk
                     echo "build"
                 }
-      }
-    }
-    stage('distribute') {
-      steps {
-        script {
-                    echo "distribute"    
+            }
         }
-      }
+        stage('distribute') {
+            steps {
+                script {
+                    echo "distribute"
+                }
+            }
+        }
     }
-  }
 }
